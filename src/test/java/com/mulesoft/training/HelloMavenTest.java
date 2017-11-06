93a1c3f60d5704d421fcd82a5ebaa2964f120803
Patro <ppatro@deloitte.com>
2017-11-06 16:38:28 -0500

    Added CloudHub plugin 2ndtime for deployment

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -2,14 +2,20 @@ package com.mulesoft.training;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
+	@Rule
+	public DynamicPort myPort = new DynamicPort("http.port");
+	
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "Hello World");
+        System.out.println("\n \n Dynamic port in Testcase 1 --------->");
+    	runFlowAndExpect("mavenFlow", "Hello World");
     }
     
     @Override
